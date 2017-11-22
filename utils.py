612ec82c8e3403577469bbb3f57357eaea8994f8
Julius Juodagalvis <julius.juodagalvis@protonmail.com>
2017-11-22 14:52:25 +0200

    Made session start-up less laborious.

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import csv
 import itertools
+import random
+
+import numpy as np
 
 def read_user_list_file(file_):
     ratings = []
@@ -16,3 +19,53 @@ def group_ratings_by_key(ratings, key):
         list_ = [itertools.chain(x[:key] + x[key+1:]) for x in group]
         ratings_grouped[grouper] = dict(list_)
     return ratings_grouped
+
+def index(dict_):
+    index = {}
+    for i, k in enumerate(dict_):
+        index[k] = i
+    return index
+
+def ratings_indices(ratings):
+    coords = []
+    values = []
+    for rating in ratings:
+        coords.append([rating[0], rating[1]])
+        values.append(rating[2])
+    return (coords, values)
+
+def _setup_for_interactive(userfile):
+    def _set_of_variables(key):
+        grouped = group_ratings_by_key(ratings, key)
+        table = index(grouped)
+        list_ = [k for k in table.keys()]
+        return grouped, table, list_
+    def calculate_mean_ratings(grouped_ratings):
+        means = []
+        for pair in grouped_ratings.values():
+            means.append([np.mean([r for r in pair.values()]) - global_mean])
+        return np.array(means)
+    ratings = read_user_list_file(userfile)
+    temp_ratings = []  #ignore any rows without rating
+    for r in ratings:
+        if len(r[2]) > 0:
+            temp_ratings.append(r)
+    ratings = temp_ratings
+    del temp_ratings
+    ratings = [[r[0], int(float(r[1])), float(r[2])] for r in ratings]
+    ratings_by_game, table_games, list_games = _set_of_variables(1)
+    ratings_by_user, table_users, list_users = _set_of_variables(0)
+    ratings_indexed = [
+        [table_users[t[0]], table_games[t[1]], t[2]] for t in ratings]
+    indices, values = ratings_indices(ratings_indexed)
+    shape = np.array([len(list_users), len(list_games)])
+    global_mean = np.mean(values)
+    means_game = calculate_mean_ratings(ratings_by_game)
+    means_user = calculate_mean_ratings(ratings_by_user)
+    return (
+        ratings,
+        ratings_by_game, table_games, list_games, means_game,
+        ratings_by_user, table_users, list_users, means_user,
+        indices, values, shape,
+        global_mean,
+    )
