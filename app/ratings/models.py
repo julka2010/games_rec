9379281234862383106feb973b2afdd5e974f44a
Julius Juodagalvis <julius.juodagalvis@protonmail.com>
2019-01-29 03:03:34 +0200

    Added ManyToMany relation between Game and Player.

--- app/ratings/models.py
+++ app/ratings/models.py
@@ -1,13 +1,6 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-class Player(models.Model):
-    bgg_username = models.CharField(
-        max_length=255,
-        default=None, blank=True, null=True
-    )
-
-
 class Game(models.Model):
     bgg_id = models.IntegerField(
         default=None, blank=True, null=True,
@@ -22,7 +15,24 @@ class Game(models.Model):
     dump = JSONField(default=dict)
 
 
+class Player(models.Model):
+    bgg_username = models.CharField(
+        max_length=255,
+        default=None, blank=True, null=True
+    )
+    objects = models.Manager()
+    games = models.ManyToManyField(Game, through='rating')
+
+    @property
+    def unplayed_games(self):
+        """Returns queryset of games player haven't played yet."""
+        played_games = self.games.values('pk')
+        return Game.objects.exclude(pk__in=played_games)
+
+
 class Rating(models.Model):
     player = models.ForeignKey(Player, models.CASCADE)
     game = models.ForeignKey(Game, models.CASCADE)
     value = models.FloatField(default=8.7)
+    class Meta:
+        unique_together = (('player', 'game'))
