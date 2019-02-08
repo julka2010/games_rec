dbc9c0d912ff170b6f35b4e4bc4ef39cee88214d
Julius Juodagalvis <julius.juodagalvis@protonmail.com>
2019-02-08 23:33:46 +0200

    Added a dummy test for keras models.

--- app/training/tests.py
+++ app/training/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+from ratings.models import Player
+from training.models import KerasSinglePlayerModel
+from training.tasks import train_player
+
+PEDRATOR = Player.objects.get(bgg_username='Pedrator')
+
+class SinglePlayerModelTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # just so correct worker gets the task
+        train_player.delay(PEDRATOR.id).wait(interval=1)
+        cls.model = KerasSinglePlayerModel.objects.last()
+        cls.ratings = PEDRATOR.ratings_set.to_dataframe(
+            ['game_id', 'player_id', 'value']
+        ).sort_values('value', ascending=True).reset_index(drop=True)
+
+    def compare_higher_test(self):
+        higher = self.ratings.iloc[-10:]
+        lower = self.ratings.iloc[:10]
+        for h, l in zip(higher.value, lower.value):
+            assert h > l, "Incorrect data fed."
+            highest, values = model.keras_model.predict(inputs)
+            self.assertGreater(highest[0], highest[1])
